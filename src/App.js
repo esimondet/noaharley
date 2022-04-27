7fc9890d6261b2e0a03d3720f67816e1d68e03f8
Edison Simondet <edison.simondet@gmail.com>
2022-04-26 21:08:57 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Placeholder from './components/placeholder';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Placeholder />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/placeholder/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+
+function Placeholder() {
+  return (
+    <Columns>
+      <section> WIP 🏗️ </section>
+    </Columns>
+  );
+}
+
+export default Placeholder;
